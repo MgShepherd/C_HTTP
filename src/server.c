a51d2e4fd1fdb1c26b018736657399000a957da0
MgShepherd <michaelshep1504@gmail.com>
2025-10-06 18:59:57 +0100

    Started accepting connections and sending data to client

--- src/server.c
+++ src/server.c
@@ -7,16 +7,22 @@
 #include <errno.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <unistd.h>
 
 static const char* PORT_NUMBER = "8080";
 static const int BACKLOG_SIZE = 5;
 
+static const char* EXAMPLE_MSG = "Hello World!\n";
+
 struct M_Server {
 	struct addrinfo* connect_addrs;
+	int socket_fd;
 };
 
+void get_string_address(const struct addrinfo* address, char* output, size_t max_len);
 int load_connect_addrs(M_Server* server);
 int create_and_bind_socket(const M_Server* server);
+int accept_connection(const M_Server* server);
 
 int m_server_start(M_Server** server) {
 	*server = malloc(sizeof(M_Server));
@@ -30,17 +36,21 @@ int m_server_start(M_Server** server) {
 		return result;
 	}
 
-	const int socketfd = create_and_bind_socket(*server);
-	if (socketfd < 0) {
-		return socketfd;
+	(*server)->socket_fd = create_and_bind_socket(*server);
+	if ((*server)->socket_fd < 0) {
+		return (*server)->socket_fd;
 	}
 
-	result = listen(socketfd, BACKLOG_SIZE);	
+	result = listen((*server)->socket_fd, BACKLOG_SIZE);
 	if (result == -1) {
 		printf("[ERROR] Unable to start listening on port %s, error: %d\n", PORT_NUMBER, errno);
 	}
 
-	printf("Started server listening on port %s...\n", PORT_NUMBER);
+	char connection_address[INET6_ADDRSTRLEN];
+	get_string_address((*server)->connect_addrs, connection_address, INET6_ADDRSTRLEN);
+	printf("Started server %s listening on port %s..\n", connection_address, PORT_NUMBER);
+
+	result = accept_connection(*server);
 
 	return 0;
 }
@@ -49,6 +59,19 @@ void m_server_destroy(M_Server* server) {
 	if (server == NULL) return;
 	freeaddrinfo(server->connect_addrs);
 	free(server);
+}
+
+void get_string_address(const struct addrinfo* address, char* output, size_t max_len) {
+	void* addr;
+	if (address->ai_family == AF_INET) {
+		addr = &((struct sockaddr_in *)address->ai_addr)->sin_addr;
+	} else if (address->ai_family == AF_INET6) {
+		addr = &((struct sockaddr_in6 *)address->ai_addr)->sin6_addr;
+	} else {
+		return;
+	}
+
+	inet_ntop(address->ai_family, addr, output, max_len);
 }
 
 int load_connect_addrs(M_Server *server) {
@@ -88,3 +111,27 @@ int create_and_bind_socket(const M_Server* server)  {
 	return socketfd;
 }
 
+int accept_connection(const M_Server *server) {
+	if (server == NULL || server->socket_fd < 0) {
+		printf("[ERROR] Unable to use existing socket to accept incoming requests\n");
+		return -1;
+	}
+
+	struct sockaddr connected_addr;
+	socklen_t addr_len = sizeof(connected_addr);
+	int connect_fd = accept(server->socket_fd, &connected_addr, &addr_len);
+	if (connect_fd == -1) {
+		printf("[ERROR] Unable to accept incoming connection, error: %d\n", errno);
+		return -1;
+	}
+
+	int result = send(connect_fd, EXAMPLE_MSG, strlen(EXAMPLE_MSG), 0);
+	if (result < 0) {
+		printf("[ERROR] Unable to send message to connected client, error: %d\n", errno);
+		return result;
+	}
+
+	close(connect_fd);
+
+	return 0;
+}
