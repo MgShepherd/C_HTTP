2d7a4496ba020bd59c55546cb72ebe00fd1edc40
Michael <michaelshep1504@gmail.com>
2025-11-17 19:15:03 +0000

    Improving constants file

--- lib/include/constants.h
+++ lib/include/constants.h
@@ -1,7 +1,7 @@
 #ifndef _CONSTANTS_H_
 #define _CONSTANTS_H_
 
-extern const char *PORT_NUMBER;
-extern const int BACKLOG_SIZE;
+#define PORT_NUMBER "8080"
+#define BACKLOG_SIZE 10
 
 #endif // !_CONSTANTS_H_
--- lib/src/constants.c
+++
@@ -1,4 +0,0 @@
-#include "constants.h"
-
-const char *PORT_NUMBER = "8080";
-const int BACKLOG_SIZE = 10;
